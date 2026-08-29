3f0eb74ee7d6ad7da0ed4e59ea75fa7ae552c918
Andrewf041 <148039474+Andrewf041@users.noreply.github.com>
2026-08-29 23:00:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,28 @@
-self.addEventListener('install', (e) => {
-    console.log('[ServiceWorker] Установлен');
+const CACHE_NAME = 'phoenix-v2';
+const URLS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    );
+    self.skipWaiting();
 });
-self.addEventListener('fetch', (e) => {});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
